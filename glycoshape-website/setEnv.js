d2e67fa8b316159ee9fcf3a42c55fe3d86211e36
Ojas_singh <singhojas007@gmail.com>
2024-01-13 15:22:03 +0000

    update

--- glycoshape-website/setEnv.js
+++ glycoshape-website/setEnv.js
@@ -2,7 +2,5 @@ const fs = require('fs');
 
 const envFile = process.argv[2] === 'production' ? '.env.production' : '.env.development';
 
-fs.copyFileSync(envFile, '.env', (err) => {
-  if (err) throw err;
-  console.log(`${envFile} was copied to .env`);
-});
+fs.copyFileSync(envFile, '.env');
+console.log(`${envFile} was copied to .env`);
