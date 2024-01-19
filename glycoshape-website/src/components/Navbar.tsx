620f09889b37b3a5b3968faa26c007116318498f
Ojas_singh <singhojas007@gmail.com>
2024-01-19 12:10:43 +0000

    update

--- glycoshape-website/src/components/Navbar.tsx
+++ glycoshape-website/src/components/Navbar.tsx
@@ -30,7 +30,7 @@ import logo from './assets/logo_white.png';
 
 const Navbar: React.FC = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
-  const isDevelopment = process.env.REACT_APP_BUILD_DEV;
+  const isDevelopment = process.env.REACT_APP_BUILD_DEV === "true";
   const { isOpen, onOpen, onClose } = useDisclosure();
   const isMobile = useBreakpointValue({ base: true, md: false });
 
--- glycoshape-website/src/components/ReGlyco.tsx
+++ glycoshape-website/src/components/ReGlyco.tsx
@@ -108,7 +108,7 @@ interface UniprotData {
 const ReGlyco = () => {
 
   const apiUrl = process.env.REACT_APP_API_URL;
-  const isDevelopment = process.env.REACT_APP_BUILD_DEV;
+  const isDevelopment = process.env.REACT_APP_BUILD_DEV === "true";
 
   const [uniprotID, setUniprotID] = useState<string>("");
   const [UniprotData, setUniprotData] = useState<UniprotData | null>(null);
